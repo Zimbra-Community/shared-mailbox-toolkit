b4ddd9d2b4a5b302eba5ae1558285c293dfcde30
Barry de Graaff <info@barrydegraaff.tk>
2016-09-20 14:49:15 +0200

    add first version of sharetoolkit_client Zimlet

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Convienient and easy creation of mailbox shares and other stuff to get family mailbox like features.
 
 This repository will track issues and code for https://www.indiegogo.com/projects/zimbra-shared-mailbox-toolkit-for-administrators/x/6463657#/updates
+
+# tk_barrydegraaff_sharetoolkit_client
+Once this Zimlet is deployed, a user who clicks on the root of a shared mailbox, will see the inbox of that share, instead of "No results found".
---
+++ tk_barrydegraaff_sharetoolkit_client/tk_barrydegraaff_sharetoolkit_client.js
@@ -0,0 +1,155 @@
+/**
+Copyright (C) 2014-2016  Barry de Graaff
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
+//Constructor
+function tk_barrydegraaff_sharetoolkit_client_HandlerObject() {
+};
+
+
+tk_barrydegraaff_sharetoolkit_client_HandlerObject.prototype = new ZmZimletBase();
+tk_barrydegraaff_sharetoolkit_client_HandlerObject.prototype.constructor = tk_barrydegraaff_sharetoolkit_client_HandlerObject;
+
+tk_barrydegraaff_sharetoolkit_client_HandlerObject.prototype.toString =
+function() {
+   return "tk_barrydegraaff_sharetoolkit_client_HandlerObject";
+};
+
+/** 
+ * Creates the Zimlet, extends {@link https://files.zimbra.com/docs/zimlet/zcs/8.6.0/jsapi-zimbra-doc/symbols/ZmZimletBase.html ZmZimletBase}.
+ * @class
+ * @extends ZmZimletBase
+ *  */
+var ShareToolkitClient = tk_barrydegraaff_sharetoolkit_client_HandlerObject;
+
+/** 
+ * This method gets called when Zimbra Zimlet framework initializes.
+ * ShareToolkitClient uses the init function to load openpgp.js and configure the user settings and runtime variables.
+ */
+ShareToolkitClient.prototype.init = function() {
+
+}
+
+
+ /**
+ * Called when a left click occurs (by the tree view listener). The folder that
+ * was clicked may be a search, since those can appear in Trash within the folder tree. The
+ * appropriate search will be performed.
+ *
+ * @param {ZmOrganizer}		folder		the folder or search that was clicked
+ * 
+ * @private
+ */
+ZmFolderTreeController.prototype._itemClicked = function(folder, openInTab) {
+
+	// bug 41196 - turn off new mail notifier if inactive account folder clicked
+	if (appCtxt.isOffline) {
+		var acct = folder.getAccount();
+		if (acct && acct.inNewMailMode) {
+			acct.inNewMailMode = false;
+			var allContainers = appCtxt.getOverviewController()._overviewContainer;
+			for (var i in allContainers) {
+				allContainers[i].updateAccountInfo(acct, true, true);
+			}
+		}
+	}
+
+	if (folder.type == ZmOrganizer.SEARCH) {
+		// if the clicked item is a search (within the folder tree), hand
+		// it off to the search tree controller
+		var stc = this._opc.getTreeController(ZmOrganizer.SEARCH);
+		stc._itemClicked(folder, openInTab);
+	} else if (folder.id == ZmFolder.ID_ATTACHMENTS) {
+		var attController = AjxDispatcher.run("GetAttachmentsController");
+		attController.show();
+	}
+    else {
+		var searchFor = ZmId.SEARCH_MAIL;
+		if (folder.isInTrash()) {
+			var app = appCtxt.getCurrentAppName();
+			// if other apps add Trash to their folder tree, set appropriate type here:
+			if (app == ZmApp.CONTACTS) {
+				searchFor = ZmItem.CONTACT;
+			}
+		}
+		var sc = appCtxt.getSearchController();
+		var acct = folder.getAccount();
+
+		var sortBy = appCtxt.get(ZmSetting.SORTING_PREF, folder.nId);
+		if (!sortBy) {
+			sortBy = (sc.currentSearch && folder.nId == sc.currentSearch.folderId) ? null : ZmSearch.DATE_DESC;
+		}
+		else {
+			//user may have saved folder with From search then switched views; don't allow From sort in conversation mode
+			var groupMode = appCtxt.getApp(ZmApp.MAIL).getGroupMailBy();
+			if (groupMode == ZmItem.CONV && (sortBy == ZmSearch.NAME_ASC || sortBy == ZmSearch.NAME_DESC)) {
+				sortBy = appCtxt.get(ZmSetting.SORTING_PREF, appCtxt.getCurrentViewId());  //default to view preference
+				if (!sortBy) {
+					sortBy = ZmSearch.DATE_DESC; //default
+				}
+				appCtxt.set(ZmSetting.SORTING_PREF, sortBy, folder.nId);
+			}
+		}
+      if ((folder._isRemote == true) && (folder.oname == "USER_ROOT"))
+      {
+         console.log("ShareToolkitClient: shared mailbox detected, rewrite query to /Inbox");
+         var query = folder.createQuery();
+         query = query.substring(0,query.length -1);
+         query = query + '/inbox"';
+         
+      }
+      else
+      {
+         var query = folder.createQuery();
+      }
+		var params = {
+			query:          query,
+			searchFor:      searchFor,
+			getHtml:        folder.nId == ZmFolder.ID_DRAFTS || appCtxt.get(ZmSetting.VIEW_AS_HTML),
+			types:          folder.nId == ZmOrganizer.ID_SYNC_FAILURES ? [ZmItem.MSG] : null, // for Sync Failures folder, always show in traditional view
+			sortBy:         sortBy,
+			accountName:    acct && acct.name,
+			userInitiated:  openInTab,
+			origin:         ZmId.SEARCH
+		};
+
+		sc.resetSearchAllAccounts();
+
+		if (appCtxt.multiAccounts) {
+			// make sure we have permissions for this folder (in case an "external"
+			// server was down during account load)
+			if (folder.link && folder.perm == null) {
+				var folderTree = appCtxt.getFolderTree(acct);
+				if (folderTree) {
+					var callback = new AjxCallback(this, this._getPermissionsResponse, [params]);
+					folderTree.getPermissions({callback:callback, folderIds:[folder.id]});
+				}
+				return;
+			}
+
+			if (appCtxt.isOffline && acct.hasNotSynced() && !acct.__syncAsked) {
+				acct.__syncAsked = true;
+
+				var dialog = appCtxt.getYesNoMsgDialog();
+				dialog.registerCallback(DwtDialog.YES_BUTTON, this._syncAccount, this, [dialog, acct]);
+				dialog.setMessage(ZmMsg.neverSyncedAsk, DwtMessageDialog.INFO_STYLE);
+				dialog.popup();
+			}
+		}
+
+		sc.search(params);
+	}
+};
