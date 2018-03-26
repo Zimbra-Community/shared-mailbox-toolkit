7260cbd5d500110ddcda4778781d16082160d2c8
barrydegraaff <info@barrydegraaff.tk>
2018-03-26 20:26:01 +0200

    Allow unlimted characters in the domain extension, .amsterdam

--- extension/ShareToolkit/src/tk/barrydegraaff/toolkit/ShareToolkitSoapHandler.java
+++ extension/ShareToolkit/src/tk/barrydegraaff/toolkit/ShareToolkitSoapHandler.java
@@ -99,7 +99,7 @@ public class ShareToolkitSoapHandler extends DocumentHandler {
     }
 
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
-            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$", Pattern.CASE_INSENSITIVE);
 
     public static boolean validate(String emailStr) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
